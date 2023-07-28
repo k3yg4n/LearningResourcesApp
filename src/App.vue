4a5ff1dbfb3141e7b32e5c782b4709e441ada18e
Keegan Liu <keeganliu5678@gmail.com>
2023-07-28 11:36:53 -0700

    Complete InterestBlock component mockup

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <interest-block
+      v-for="int in storedInterests"
+      :key="int.id"
+      :title="int.title"
+      :description="int.description"
+      :link="int.link"
+    ></interest-block>
+  </ul>
+</template>
+
+<script>
+import Interest from './components/interest/InterestBlock.vue';
+export default {
+  components: {
+    'interest-block': Interest,
+  },
+  data() {
+    return {
+      storedInterests: [
+        {
+          id: 'favourite-album',
+          title: 'My favourite album',
+          description: 'This is my favourite album.',
+          link: 'https://google.com',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/interest/InterestBlock.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete Interest</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">Learn more...</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    description: String,
+    link: String,
+  },
+};
+</script>
